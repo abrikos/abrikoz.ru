c0265cd60274db82b519573222995ec324eedf2d
Artem Filippow <a.filippov@qtech.ru>
2026-08-26 21:34:45 +0900

    delete old archives

--- db-backup.sh
+++ db-backup.sh
@@ -3,3 +3,4 @@
 docker exec mongodb mongodump
 docker cp mongodb:/dump /home/abrikos
 tar -czf "/home/abrikos/backup/$( date +%Y%m%d_%H%M%S ).tar.gz" /home/abrikos/dump
+find /home/abrikos/backup -mtime +10 -type f -delete
