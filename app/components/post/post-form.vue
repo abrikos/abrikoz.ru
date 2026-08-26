90337ae7e60db88e290009d09d5a294dd365a856
Artem Filippow <a.filippov@qtech.ru>
2026-08-26 17:48:59 +0900

    vuetify post form

--- app/components/post/post-form.vue
+++ app/components/post/post-form.vue
@@ -32,32 +32,28 @@ function upload(){
                 v-icon(icon="mdi-image")
             v-textarea(v-model="post.short" label="Short" auto-grow)
 
-
+            v-btn(size="sm" icon="mdi-pencil" :to="`/post-edit-${post.id}`")
             v-textarea(v-model="post.text" label="Body (accepts markdown syntax)" auto-grow)
               template(v-slot:append)
-                v-icon(icon="mdi-help")
-                  q-popup-proxy
-                    q-banner
-                      a(href="https://skillbox.ru/media/code/yazyk-razmetki-markdown-shpargalka-po-sintaksisu-s-primerami/" target="_blank") Markdown syntax
-                q-tooltip Markdown syntax
+
+                  v-dialog(max-width="500" activator="parent" )
+                    template(v-slot:activator="{props}")
+                      v-icon(icon="mdi-help" v-bind="props")
+                    template(v-slot:default="{isActive}")
+                      v-card
+                        v-card-title.d-flex.justify-space-between.align-center
+                          v-icon(icon="mdi-help")
+                          v-icon(icon="mdi-close" @click="isActive.value=false")
+                        v-card-text
+                          a(href="https://skillbox.ru/media/code/yazyk-razmetki-markdown-shpargalka-po-sintaksisu-s-primerami/" target="_blank") Markdown syntax
             div.row.items-center
               div.col
-                q-toggle( v-model="post.published" label="Show for all")
+                v-checkbox(v-model="post.published" label="Show for all")
               div.col.text-right {{ post.date  }}
 
-          q-card-actions.flex.justify-between
-            q-btn(type="submit" color="primary" :flat="false" :label="route.params.id ? 'Save':'Create'")
-            q-btn(type="reset" :flat="false" label="Reset" v-if="route.params.id")
-      //q-file(v-model="post.poster" @update:model-value="upload" label="Upload images" multiple)
-      div.images.flex
-        div.q-pa-sm(v-for="image of post.images")
-          div.image.flex.items-center
-            img(:src="image")
-          div
-            q-btn(size="sm" icon="mdi-image" @click="post.poster=image")
-              q-tooltip Poster image
-            q-btn(size="sm" icon="mdi-text" @click="addImage(image)")
-              q-tooltip In text
+          v-card-actions.d-flex.justify-space-between
+            v-btn(type="submit" color="primary") {{route.params.id ? 'Save':'Create'}}
+            v-btn(type="reset" :flat="false" v-if="route.params.id") Reset
 
     div.w-50
       router-link(:to="`/post-view-${route.params.id}`") Go to post
